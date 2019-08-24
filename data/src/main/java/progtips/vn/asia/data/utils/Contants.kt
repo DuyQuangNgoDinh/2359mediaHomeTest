4fe9e2a84d4f0e79355821c5d57637b900a127f7
DuyQuangNgoDinh <ngodinhduyquang@gmail.com>
2019-08-24 11:23:40 +0700

    Update size of placeholder's poster image's height from 800 to 900 and change the poster_size of the url from themoviedb.org to w342

--- data/src/main/java/progtips/vn/asia/data/utils/Contants.kt
+++ data/src/main/java/progtips/vn/asia/data/utils/Contants.kt
@@ -5,4 +5,4 @@ const val SITE_URL: String = "https://api.themoviedb.org"
 const val VERSION: String = "/3/movie/"
 const val BASE_URL: String = SITE_URL + VERSION
 
-const val IMG_URL: String = "https://image.tmdb.org/t/p/w185/"
+const val IMG_URL: String = "https://image.tmdb.org/t/p/w342/"
